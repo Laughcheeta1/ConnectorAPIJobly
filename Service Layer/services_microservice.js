480200f3428410fac306b09e19f9016843155615
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2024-03-09 17:45:39 -0500

    Finished services microservice service

--- Service Layer/services_microservice.js
+++ Service Layer/services_microservice.js
@@ -1,5 +1,8 @@
 import {contract_microservices as route} from './microservices_routes.js';
 const axios = require('axios');
+/*
+    Version 1.0
+*/
 
 
 // Provider point of view
@@ -17,23 +20,25 @@ const getServiceForProvider = (service_id) => {
 }
 
 
-
-
 // Employer point of view
-const makeNewOffer = (offer) => {
-    return axios.post(`${route}/new_offer`, offer);
+const createNewService = (service) => {
+    return axios.post(`${route}/create_service`, service);
 }
 
 
 const getServicesByEmployer = (employer_id) => {
-    return axios.get(`${route}/services/employer/${employer_id}`);
+    return axios.get(`${route}/get_all_employer/${employer_id}`);
 
 }
 
-const getSpecificService = (service_id) => {
-    return axios.get(`${route}/services/${id}`);
+const getServiceForEmployers = (service_id) => {
+    return axios.get(`${route}/get_one_employer/${id}`);
 }
 
-const getServicesOfferingsByEmployer = (id) => {
-    return axios.get(`${route}/services_provider/${id}`);
+const deleteService = (service_id) => {
+    return axios.delete(`${route}/${id}`);
+}
+
+const getPostulations = (service_id) => {
+    return axios.get(`${route}/${service_id}/postulations`);
 }
